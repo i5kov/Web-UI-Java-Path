55faac96774e0182f865f3c9418de466fa56a737
Ivan Petkov <ipetkow90@gmail.com>
2020-02-03 16:42:24 +0200

    Refactoring BaseTest class. - adding annotations

--- 06 - Selenium WebDriver with Java/src/test/java/base/BaseTest.java
+++ 06 - Selenium WebDriver with Java/src/test/java/base/BaseTest.java
@@ -4,28 +4,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
 
 public class BaseTest {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        driver.findElement(By.linkText("Shifting Content")).click();
-        driver.findElement(By.linkText("Example 1: Menu Element")).click();
-        String h3Title = driver.findElement(By.tagName("h3")).getText();
-        int linksCount = driver.findElements(By.cssSelector("ul li > a")).size();
-        System.out.println(String.format("Links at [%s] page are %d", h3Title, linksCount));
-        driver.quit();
+        homePage = new HomePage(driver);
     }
 
-    public static void main(String[] args) {
-        BaseTest baseTest = new BaseTest();
-        baseTest.setUp();
+    @BeforeMethod
+    public void goToHomePage() {
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 }
