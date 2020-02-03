8516ab7e86b042b1f6a973f38eac09a053f0a2c8
Ivan Petkov <ipetkow90@gmail.com>
2020-02-03 16:43:47 +0200

    First Page Objects created. - HomePage class representing home page of the app - LoginPage class representing login page of the app - SecureAreaPage class representing the page after login successfully

---
+++ 06 - Selenium WebDriver with Java/src/main/java/pages/SecureAreaPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SecureAreaPage {
+
+    private WebDriver driver;
+    private By messageAfterLogin = By.id("flash");
+
+    public SecureAreaPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getMessageAfterLogin() {
+        return driver.findElement(messageAfterLogin).getText();
+    }
+}
