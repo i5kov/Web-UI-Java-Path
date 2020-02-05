46661c0cfee8cc8fb032f21514bdd55cee3a786b
Ivan Petkov <ipetkow90@gmail.com>
2020-02-05 14:03:46 +0200

    refactor

--- 06 - Selenium WebDriver with Java/src/test/java/slider/SliderTests.java
+++ 06 - Selenium WebDriver with Java/src/test/java/slider/SliderTests.java
@@ -13,10 +13,10 @@ public class SliderTests extends BaseTest {
         HorizontalSliderPage horizontalSliderPage = homePage.clickHorizontalSliderLink();
         String expectedMainHeaderPage = "Horizontal Slider";
         assertEquals(horizontalSliderPage.getMainHeaderPageText(), expectedMainHeaderPage,
-                    String.format("You are not at '%s' page", expectedMainHeaderPage));
+                String.format("You are not at '%s' page", expectedMainHeaderPage));
         String sliderValue = "4";
         horizontalSliderPage.moveSliderPointer(sliderValue);
         assertEquals(horizontalSliderPage.getSliderRange(), sliderValue,
-                    "Selected range on the slider is not correct!");
+                "Selected range on the slider is not correct!");
     }
 }
